7238182acb280382e736df262ea5d189e534a153
rakesh gyawali <rakeshgyawali@gmail.com>
2021-07-21 17:10:48 +0545

    project completed

---
+++ app/src/main/java/com/rakesh_gyawali/covid_19tracker/DetailActivity.java
@@ -0,0 +1,56 @@
+package com.rakesh_gyawali.covid_19tracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private int positionCountry;
+    TextView tvCountry, tvCases, tvTodayCases, tvDeaths, tvTodayDeaths, tvRecovered, tvCritical, tvActive, tvTests;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        positionCountry = intent.getIntExtra("position", -1);
+
+        getSupportActionBar().setTitle("Details of " + AffectedCountries.countryModelsList.get(positionCountry).getCountry());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true); //Without this it works fine too.
+
+        tvCountry = findViewById(R.id.tvCountry);
+        tvCases = findViewById(R.id.tvCases);
+        tvTodayCases = findViewById(R.id.tvTodayCases);
+        tvDeaths = findViewById(R.id.tvDeaths);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvTests = findViewById(R.id.tvTests);
+
+        tvCountry.setText(AffectedCountries.countryModelsList.get(positionCountry).getCountry());
+        tvCases.setText(AffectedCountries.countryModelsList.get(positionCountry).getCases());
+        tvTodayCases.setText(AffectedCountries.countryModelsList.get(positionCountry).getTodayCases());
+        tvDeaths.setText(AffectedCountries.countryModelsList.get(positionCountry).getDeaths());
+        tvTodayDeaths.setText(AffectedCountries.countryModelsList.get(positionCountry).getTodayDeaths());
+        tvRecovered.setText(AffectedCountries.countryModelsList.get(positionCountry).getRecovered());
+        tvCritical.setText(AffectedCountries.countryModelsList.get(positionCountry).getCritical());
+        tvActive.setText(AffectedCountries.countryModelsList.get(positionCountry).getActive());
+        tvTests.setText(AffectedCountries.countryModelsList.get(positionCountry).getTests());
+    }
+    // For home button in Support Action Bar.
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+}
